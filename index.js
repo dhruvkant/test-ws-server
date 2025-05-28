f78c0af00cf7b4ca92f234610b64dcf4dfd401db
Dhruv Kant <dhruv.kant@nagarro.com>
2025-05-28 16:01:42 +0530

    express import updated.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+import express from "express";
 import WebSocket, { WebSocketServer } from "ws";
 
-var express = require("express");
 var app = express();
 app.listen(3333, () => {
   console.log("Server running on port 3333");
